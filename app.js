4f97c3dbf5ff9caf4e8414c5beaab76b8aa8702b
Aqib2110 <muhammadaqib2110@gmail.com>
2025-03-03 13:59:22 +0500

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cors({
-origin:["https://otp-frontend-rust.vercel.app"],
+origin:["https://otp-frontend-rust.vercel.app/"],
 methods:["GET","POST"],
 credentials:true
 }));
