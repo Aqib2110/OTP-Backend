b0b1d123008cbebf68678bfc6f22c041711dcb0e
Aqib2110 <muhammadaqib2110@gmail.com>
2025-03-03 14:07:25 +0500

    Update app.js

--- app.js
+++ app.js
@@ -5,9 +5,10 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cors({
-origin:["https://otp-frontend-rust.vercel.app/"],
-methods:["GET","POST"],
-credentials:true
+origin:["https://otp-frontend-rust.vercel.app"],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true 
 }));
 app.options('*', cors());
 var genotp;
