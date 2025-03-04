2ce9a702f29a7b02fe26c86771dab1796d241c32
Aqib2110 <muhammadaqib2110@gmail.com>
2025-03-04 12:28:42 +0500

    Update Nodemailer.js

--- Nodemailer.js
+++ Nodemailer.js
@@ -1,15 +1,18 @@
 const nodemailer = require('nodemailer');
 require('dotenv').config();
+var EMAIL_USER = 'muhammadaqib8447@gmail.com'
+var EMAIL_PASS = 'bbdicrkimxuxazlj'
 function mailsender(email,otp){
     let transporter = nodemailer.createTransport({
         service: 'Gmail',
         auth: {
-            user: process.env.EMAIL_USER,
-            pass: process.env.EMAIL_PASS,
+            user: EMAIL_USER,
+            pass: EMAIL_PASS,
         }
     });
+    ////////////////////////////////////////////////////////////////////////////////// process.env.EMAIL_PASS,
     let mailOptions = {
-        from:process.env.EMAIL_USER,
+        from:EMAIL_USER,
         // to: 'muhammadaqib29062003@gmail.com',
         to: email,
         subject: 'Email Verification',
